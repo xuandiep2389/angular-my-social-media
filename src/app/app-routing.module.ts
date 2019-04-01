94e02547d51f54e861edbaaf42c436e5a4ca576a
vu linh <xuandiep2389@gmail.com>
2019-04-01 09:49:08 +0700

    view post detail done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PostDetailComponent} from './post-detail/post-detail.component';
+import {PostsComponent} from './posts/posts.component';
 
 const routes: Routes = [
-  { path: 'detail/:id', component: PostDetailComponent }
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: PostsComponent},
+  { path: 'detail/:id', component: PostDetailComponent },
+
+
 ];
 
 @NgModule({
